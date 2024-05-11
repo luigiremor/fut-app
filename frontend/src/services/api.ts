5b0976f46e1c3ceb3b8813418fdc7f1195d1ab9b
luigi remor <luigiremor@gmail.com>
2024-05-11 15:16:40 -0300

    chore: Add frontend configuration files and dependencies

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+  // TODO: Change this to the actual API URL through environment variables
+  baseURL: 'http://localhost:8080/api/'
+});
+
+export default api;
